eb26130b16c9bcfc687f0790fe1dcde771b15356
shane howe <shanehowe100@hotmail.com>
2024-01-31 10:14:04 +0000

    Test Robot class finished

--- Robot/src/Robot.java
+++ Robot/src/Robot.java
@@ -1,15 +1,25 @@
+
+
 public class Robot {
 
     private String name;
     private boolean working;
 
     public Robot(String name) {
-        this.name = name;
+        setName(name);
         this.working = false;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.name = name;
     }
 
     public boolean isWorking() {
@@ -18,5 +28,13 @@ public class Robot {
 
     public void turnOn() {
         working = true;
+    }
+
+    public String getWorkingMessage() {
+        if (!isWorking()) {
+            throw new IllegalStateException();
+        }
+
+        return "I am in working mode!";
     }
 }
--- Robot/tests/RobotTest.java
+++ Robot/tests/RobotTest.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class RobotTest {
 
-    Robot buddy = new Robot("buddy");
+    Robot buddy;
+
+    @BeforeEach
+    public void setUp() {
+        buddy = new Robot("buddy");
+    }
 
     @Test
     public void test_getName() {
@@ -13,7 +17,29 @@ class RobotTest {
     }
 
     @Test
+    public void test_getName_throwsIllegalArgumentExceptionWhenEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> new Robot(""));
+    }
+
+    @Test
     public void test_getWorkingIsFalseOnCreation() {
         assertFalse(buddy.isWorking());
+    }
+
+    @Test
+    public void test_turnOnSetsWorkingToTrue() {
+        buddy.turnOn();
+        assertTrue(buddy.isWorking());
+    }
+
+    @Test
+    public void test_getWorkingMessageWhenInWorkingMode() {
+        buddy.turnOn();
+        assertEquals("I am in working mode!", buddy.getWorkingMessage());
+    }
+
+    @Test
+    public void test_getWorkingMessage_throwsExceptionWhenNotInWorkingMode() {
+        assertThrows(IllegalStateException.class, () -> buddy.getWorkingMessage());
     }
 }
