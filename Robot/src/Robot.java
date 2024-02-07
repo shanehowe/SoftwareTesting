fc4497fbdd593993085f53d487255a03c3374cf9
shane howe <shanehowe100@hotmail.com>
2024-02-07 10:44:26 +0000

    Add parameterised testing and new methods in Robot

--- Robot/src/Robot.java
+++ Robot/src/Robot.java
@@ -4,8 +4,10 @@ public class Robot {
 
     private String name;
     private boolean working;
+    private int age;
 
-    public Robot(String name) {
+    public Robot(String name, int age) {
+        setAge(age);
         setName(name);
         this.working = false;
     }
@@ -22,6 +24,13 @@ public class Robot {
         this.name = name;
     }
 
+    private void setAge(int age) {
+        if (age > 30) {
+            throw new IllegalArgumentException();
+        }
+        this.age = age;
+    }
+
     public boolean isWorking() {
         return working;
     }
@@ -30,11 +39,29 @@ public class Robot {
         working = true;
     }
 
+    public void turnOff() {
+        working = false;
+    }
+
     public String getWorkingMessage() {
         if (!isWorking()) {
             throw new IllegalStateException();
         }
 
         return "I am in working mode!";
+    }
+
+    public String checkAge() {
+        return (age <= 20) ? "Age Ok" : "Too old";
+    }
+
+    public double checkCost() {
+        if (age <= 5) {
+            return 10000.00;
+        } else if (age <= 10) {
+            return 7500.00;
+        } else {
+            return 5000.00;
+        }
     }
 }
